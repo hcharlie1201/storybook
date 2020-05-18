ef8fb695e6960c36b158f5fe97c95210c88527f5
cw <carlsonjin17@gmail.com>
2020-05-18 13:41:48 -0700

    123

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:'mongodb://localhost/storybooks-dev',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
+  googleClientID:'548282302971-vfejf8sup601vvrjf3chmsgs4gc3f1gm.apps.googleusercontent.com',
+  googleClientSecret:'PDUn_rFN77NvCkztxIOyPqxj'
 }
